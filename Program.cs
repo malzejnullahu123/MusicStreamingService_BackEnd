1495d384285ac6960d69a30e791a27f307ebd92a
admin <malzejnullahu939@gmail.com>
2024-04-29 04:14:18 +0200

    CORS for new domain

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("https://music-streaming-alpha.vercel.app", "http://localhost:3000")
+            builder.WithOrigins("https://www.beatflow.live", "http://localhost:3000")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
